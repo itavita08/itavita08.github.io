8c8f888e4fe4090bfcbf89b408643f24ab70590b
YeongJun <dudwns10000@gmail.com>
2023-03-03 00:48:26 +0900

    사이드바 생성
    사이드바 css 및 열고 닫기 기능 구현

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// ============================= main show y hidden =============================
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+// ======================= menu show ===========================
+// validate if constant exists
+if(navToggle){
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+// ======================= menu hidden ===========================
+// validate if constant exists
+if(navClose){
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+// ======================= remove menu ===========================
